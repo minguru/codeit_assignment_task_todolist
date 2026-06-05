61d4760e4ef67603d73bd5d2e5dc9a73d9b42728
박민규 <aldrb0306@gmail.com>
2026-06-05 18:00:37 +0900

    fix: header icon click
    클릭 시 새로고침 안되던 내용 수정

--- app/components/global/Header.tsx
+++ app/components/global/Header.tsx
@@ -1,17 +1,21 @@
-import Link from 'next/link'
+'use client'
+
+import { useRouter } from "next/navigation"
 
 export default function Header() {
+  const router = useRouter()
+
   const headerIconAlt = "코드잇 아이콘";
 
   return (
     <header className="max-w-full w-full bg-[var(--header-background)] border-b-1 border-solid border-[var(--header-border-bottom)]">
       <div className="wrapper py-2.5 flex">
-        <Link href="/">
+        <a href="/" onClick={() => {router.push('/')}}>
           <picture>
             <source media="(max-width: 640px)" srcSet="/images/header-small.png 1x, /images/header-small@2x.png 2x, /images/header-small@3x.png 3x" />
             <img src="/images/header-large.png" srcSet="/images/header-large.png 1x, /images/header-large@2x.png 2x, /images/header-large@3x 3x" alt={headerIconAlt} />
           </picture>
-        </Link>
+        </a>
       </div>
     </header>
   )
